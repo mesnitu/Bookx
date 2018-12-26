a6e42277c519ea8446da31afd98856bdc26e94db
mesnitu <dasstech@gmail.com>
2018-12-26 22:39:15 +0000

    Update: Add AdminRequestSanitizer

--- ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/extra_datafiles/bookx_sanitizer_fields.php
+++ ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/extra_datafiles/bookx_sanitizer_fields.php
@@ -1,4 +1,38 @@
 <?php
 $sanitizer = AdminRequestSanitizer::getInstance();
-$group = array('author_description', 'publisher_description', 'series_description', 'imprint_description');
+
+$group = array(
+    'author_description', 
+    'publisher_description', 
+    'series_description', 
+    'imprint_description'
+    );
 $sanitizer->addSimpleSanitization('PRODUCT_DESC_REGEX', $group);
+
+$group = array(
+    'bookx_binding_id', 
+    'bookx_author_id', 
+    'bookx_publisher_id', 
+    'bookx_series_id', 
+    'bookx_author_type_id',
+    'bookx_genre_id',
+    'bookx_imprint_id',
+    'bookx_printing_id',
+    
+);
+$sanitizer->addSimpleSanitization('CONVERT_INT', $group);
+
+$group = array(
+    'bookx_publisher_name', 
+    'bookx_author_name', 
+    'bookx_author_type_name', 
+    'bookx_publisher_name', 
+    'bookx_genre_name',
+    'products_subtitle',
+    'pages',
+    'isbn',
+    'volume',
+    'size'
+);
+$sanitizer->addSimpleSanitization('WORDS_AND_SYMBOLS_REGEX', $group);
+
