80aef16a8f0cad765682207218608a2e9f3a2244
mesnitu <dasstech@gmail.com>
2018-12-27 23:40:18 +0000

    FIX: added sanitizer for authors and genres arrays on  'new_product_preview', 'insert_product', 'new_product'

--- ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/extra_datafiles/bookx_sanitizer_fields.php
+++ ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/extra_datafiles/bookx_sanitizer_fields.php
@@ -1,4 +1,7 @@
 <?php
+
+if (class_exists('AdminRequestSanitizer')) {
+
 $sanitizer = AdminRequestSanitizer::getInstance();
 
 $group = array(
@@ -10,16 +13,16 @@ $group = array(
 $sanitizer->addSimpleSanitization('PRODUCT_DESC_REGEX', $group);
 
 $group = array(
-    'bookx_binding_id', 
-    'bookx_author_id', 
+    'bookx_binding_id',
     'bookx_publisher_id', 
     'bookx_series_id', 
-    'bookx_author_type_id',
-    'bookx_genre_id',
     'bookx_imprint_id',
     'bookx_printing_id',
-    
+    'blank_bookx_author_id',
+    'blank_bookx_genre_id',
+    'blank_bookx_author_type_id'
 );
+
 $sanitizer->addSimpleSanitization('CONVERT_INT', $group);
 
 $group = array(
@@ -36,3 +39,34 @@ $group = array(
 );
 $sanitizer->addSimpleSanitization('WORDS_AND_SYMBOLS_REGEX', $group);
 
+$group = array(
+    'bookx_author_id' => array(
+        'sanitizerType' => 'CONVERT_INT', 
+        'method' => 'both',
+        'pages' => array('new_product_preview', 'insert_product', 'new_product'), 
+        ),
+    'bookx_author_type_id' => array(
+        'sanitizerType' => 'CONVERT_INT', 
+        'method' => 'both',
+        'pages' => array('new_product_preview', 'insert_product', 'new_product'), 
+        ),
+    'assigned_author_db_id' => array(
+        'sanitizerType' => 'CONVERT_INT', 
+        'method' => 'both',
+        'pages' => array('new_product_preview', 'insert_product', 'new_product'), 
+        ),
+    'assigned_genre_db_id' => array(
+        'sanitizerType' => 'CONVERT_INT', 
+        'method' => 'both',
+        'pages' => array('new_product_preview', 'insert_product', 'new_product'), 
+        ),
+    'bookx_genre_id' => array(
+        'sanitizerType' => 'CONVERT_INT', 
+        'method' => 'both',
+        'pages' => array('new_product_preview', 'insert_product', 'new_product'), 
+        )
+    );
+
+$sanitizer->addComplexSanitization($group);
+
+}
