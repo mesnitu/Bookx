884e82c82a9a9adfd63e3c5476da732190c29739
mesnitu <dasstech@gmail.com>
2018-12-28 18:21:53 +0000

    Update to copy_product. This file as to be renamed later

--- ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/modules/product_bookx/copy_to_confirm.php
+++ ZC_INSTALLATION/[RENAME_TO_YOUR_ADMIN_FOLDER]/includes/modules/product_bookx/copy_to_confirm.php
@@ -26,64 +26,77 @@ if (!defined('IS_ADMIN_FLAG')) {
           $categories_id = zen_db_prepare_input($_POST['categories_id']);
 
 // Copy attributes to duplicate product
-          $products_id_from=$products_id;
+  $products_id_from = $products_id;
 
           if ($_POST['copy_as'] == 'link') {
             if ($categories_id != $current_category_id) {
-              $check = $db->Execute("select count(*) as total
-                                     from " . TABLE_PRODUCTS_TO_CATEGORIES . "
-                                     where products_id = '" . (int)$products_id . "'
-                                     and categories_id = '" . (int)$categories_id . "'");
+      $check = $db->Execute("SELECT COUNT(*) AS total
+                             FROM " . TABLE_PRODUCTS_TO_CATEGORIES . "
+                             WHERE products_id = " . (int)$products_id . "
+                             AND categories_id = " . (int)$categories_id);
               if ($check->fields['total'] < '1') {
-                $db->Execute("insert into " . TABLE_PRODUCTS_TO_CATEGORIES . "
-                                          (products_id, categories_id)
-                              values ('" . (int)$products_id . "', '" . (int)$categories_id . "')");
+        $db->Execute("INSERT INTO " . TABLE_PRODUCTS_TO_CATEGORIES . " (products_id, categories_id)
+                      VALUES ('" . (int)$products_id . "', '" . (int)$categories_id . "')");
+
+        zen_record_admin_activity('Product ' . (int)$products_id . ' copied as link to category ' . (int)$categories_id . ' via admin console.', 'info');
               }
             } else {
               $messageStack->add_session(ERROR_CANNOT_LINK_TO_SAME_CATEGORY, 'error');
             }
           } elseif ($_POST['copy_as'] == 'duplicate') {
             $old_products_id = (int)$products_id;
-            $product = $db->Execute("select products_type, products_quantity, products_model, products_image,
+    $product = $db->Execute("SELECT products_type, products_quantity, products_model, products_image,
                                             products_price, products_virtual, products_date_available, products_weight,
                                             products_tax_class_id, manufacturers_id,
                                             products_quantity_order_min, products_quantity_order_units, products_priced_by_attribute,
                                             product_is_free, product_is_call, products_quantity_mixed,
                                             product_is_always_free_shipping, products_qty_box_status, products_quantity_order_max, products_sort_order,
                                             products_price_sorter, master_categories_id
-                                     from " . TABLE_PRODUCTS . "
-                                     where products_id = '" . (int)$products_id . "'");
+                             FROM " . TABLE_PRODUCTS . "
+                             WHERE products_id = " . (int)$products_id);
+
+    // fix Product copy from if Unit is 0
+    if ($product->fields['products_quantity_order_units'] == 0) {
+      $sql = "UPDATE " . TABLE_PRODUCTS . "
+              SET products_quantity_order_units = 1
+              WHERE products_id = " . (int)$products_id;
+      $results = $db->Execute($sql);
+    }
+    // fix Product copy from if Minimum is 0
+    if ($product->fields['products_quantity_order_min'] == 0) {
+      $sql = "UPDATE " . TABLE_PRODUCTS . "
+              SET products_quantity_order_min = 1
+              WHERE products_id = " . (int)$products_id;
+      $results = $db->Execute($sql);
+    }
 
             $tmp_value = zen_db_input($product->fields['products_quantity']);
-            $products_quantity = (!zen_not_null($tmp_value) || $tmp_value=='' || $tmp_value == 0) ? 0 : $tmp_value;
+    $products_quantity = (!zen_not_null($tmp_value) || $tmp_value == '' || $tmp_value == 0) ? 0 : $tmp_value;
             $tmp_value = zen_db_input($product->fields['products_price']);
-            $products_price = (!zen_not_null($tmp_value) || $tmp_value=='' || $tmp_value == 0) ? 0 : $tmp_value;
+    $products_price = (!zen_not_null($tmp_value) || $tmp_value == '' || $tmp_value == 0) ? 0 : $tmp_value;
             $tmp_value = zen_db_input($product->fields['products_weight']);
-            $products_weight = (!zen_not_null($tmp_value) || $tmp_value=='' || $tmp_value == 0) ? 0 : $tmp_value;
+    $products_weight = (!zen_not_null($tmp_value) || $tmp_value == '' || $tmp_value == 0) ? 0 : $tmp_value;
 
-            $db->Execute("insert into " . TABLE_PRODUCTS . "
-                                      (products_type, products_quantity, products_model, products_image,
+    $db->Execute("INSERT INTO " . TABLE_PRODUCTS . " (products_type, products_quantity, products_model, products_image,
                                        products_price, products_virtual, products_date_added, products_date_available,
                                        products_weight, products_status, products_tax_class_id,
-                                       manufacturers_id,
-                                       products_quantity_order_min, products_quantity_order_units, products_priced_by_attribute,
-                                       product_is_free, product_is_call, products_quantity_mixed,
-                                       product_is_always_free_shipping, products_qty_box_status, products_quantity_order_max, products_sort_order,
-                                       products_price_sorter, master_categories_id
-                                       )
-                          values ('" . zen_db_input($product->fields['products_type']) . "',
+                                                      manufacturers_id, products_quantity_order_min, products_quantity_order_units,
+                                                      products_priced_by_attribute, product_is_free, product_is_call, products_quantity_mixed,
+                                                      product_is_always_free_shipping, products_qty_box_status, products_quantity_order_max,
+                                                      products_sort_order, products_price_sorter, master_categories_id)
+                  VALUES ('" . zen_db_input($product->fields['products_type']) . "',
                                   '" . $products_quantity . "',
                                   '" . zen_db_input($product->fields['products_model']) . "',
                                   '" . zen_db_input($product->fields['products_image']) . "',
                                   '" . $products_price . "',
                                   '" . zen_db_input($product->fields['products_virtual']) . "',
                                   now(),
-                                  '" . (zen_not_null(zen_db_input($product->fields['products_date_available'])) ? zen_db_input($product->fields['products_date_available']) : '0001-01-01 00:00:00') . "',
+                          " . (zen_not_null(zen_db_input($product->fields['products_date_available'])) ? "'" . zen_db_input($product->fields['products_date_available']) . "'" : 'null') . ",
                                   '" . $products_weight . "', '0',
                                   '" . (int)$product->fields['products_tax_class_id'] . "',
                                   '" . (int)$product->fields['manufacturers_id'] . "',
-                                  '" . zen_db_input($product->fields['products_quantity_order_min']) . "',
-                                  '" . zen_db_input($product->fields['products_quantity_order_units']) . "',
+                          '" . zen_db_input(($product->fields['products_quantity_order_min'] == 0 ? 1 : $product->fields['products_quantity_order_min'])) . "',
+                          '" . zen_db_input(($product->fields['products_quantity_order_units'] == 0 ? 1 : $product->fields['products_quantity_order_units'])) . "',
                                   '" . zen_db_input($product->fields['products_priced_by_attribute']) . "',
                                   '" . (int)$product->fields['product_is_free'] . "',
                                   '" . (int)$product->fields['product_is_call'] . "',
@@ -93,23 +106,23 @@ if (!defined('IS_ADMIN_FLAG')) {
                                   '" . zen_db_input($product->fields['products_quantity_order_max']) . "',
                                   '" . zen_db_input($product->fields['products_sort_order']) . "',
                                   '" . zen_db_input($product->fields['products_price_sorter']) . "',
-                                  '" . zen_db_input($categories_id) .
-                                  "')");
+                          '" . zen_db_input($categories_id) . "')");
 
             $dup_products_id = $db->Insert_ID();
 
-            if (isset($_POST['copy_media']) && $_POST['copy_media'] == 'on') {
-              $product_media = $db->Execute("select media_id from " . TABLE_MEDIA_TO_PRODUCTS . "
-                                             where product_id = '" . (int)$products_id . "'");
-              while (!$product_media->EOF) {
-                $db->Execute("insert into " . TABLE_MEDIA_TO_PRODUCTS . "
-                              (media_id, product_id)
-                              values (
-                              '" . $product_media->fields['media_id'] . "',
-                              '" . $dup_products_id . "')");
-                $product_media->MoveNext();
+    $descriptions = $db->Execute("SELECT language_id, products_name, products_description, products_url
+                                  FROM " . TABLE_PRODUCTS_DESCRIPTION . "
+                                  WHERE products_id = " . (int)$products_id);
+    foreach ($descriptions as $description) {
+      $db->Execute("INSERT INTO " . TABLE_PRODUCTS_DESCRIPTION . " (products_id, language_id, products_name, products_description, products_url, products_viewed)
+                    VALUES ('" . (int)$dup_products_id . "',
+                            '" . (int)$description['language_id'] . "',
+                            '" . zen_db_input($description['products_name']) . "',
+                            '" . zen_db_input($description['products_description']) . "',
+                            '" . zen_db_input($description['products_url']) . "',
+                            '0')");
               }
-            }
+            
 			//////////////////////// Copy extra product type data //////////////////////////////
             $bookx_extra = $db->Execute('SELECT bookx_publisher_id, bookx_series_id, bookx_imprint_id, bookx_binding_id, bookx_printing_id, bookx_condition_id, publishing_date, pages, volume, size, isbn from ' . TABLE_PRODUCT_BOOKX_EXTRA .
             							' WHERE products_id = "' . (int)$products_id  .'"');
@@ -168,19 +181,18 @@ if (!defined('IS_ADMIN_FLAG')) {
               $description->MoveNext();
             }
 
-            $db->Execute("insert into " . TABLE_PRODUCTS_TO_CATEGORIES . "
-                          (products_id, categories_id)
-                          values ('" . (int)$dup_products_id . "', '" . (int)$categories_id . "')");
+    $db->Execute("INSERT INTO " . TABLE_PRODUCTS_TO_CATEGORIES . " (products_id, categories_id)
+                  VALUES ('" . (int)$dup_products_id . "', '" . (int)$categories_id . "')");
             $products_id = $dup_products_id;
-            $description->MoveNext();
+
 // FIX HERE
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Copy attributes to duplicate product
 // moved above            $products_id_from=zen_db_input($products_id);
-            $products_id_to= $dup_products_id;
+    $products_id_to = $dup_products_id;
             $products_id = $dup_products_id;
 
-if ( $_POST['copy_attributes']=='copy_attributes_yes' and $_POST['copy_as'] == 'duplicate' ) {
+    if (!empty($_POST['copy_attributes']) && $_POST['copy_attributes'] == 'copy_attributes_yes') {
   // $products_id_to= $copy_to_products_id;
   // $products_id_from = $pID;
 //            $copy_attributes_delete_first='1';
@@ -188,27 +200,26 @@ if ( $_POST['copy_attributes']=='copy_attributes_yes' and $_POST['copy_as'] == '
 //            $copy_attributes_duplicates_overwrite='0';
 
             if (DOWNLOAD_ENABLED == 'true') {
-              $copy_attributes_include_downloads='1';
-              $copy_attributes_include_filename='1';
+        $copy_attributes_include_downloads = '1';
+        $copy_attributes_include_filename = '1';
             } else {
-              $copy_attributes_include_downloads='0';
-              $copy_attributes_include_filename='0';
+        $copy_attributes_include_downloads = '0';
+        $copy_attributes_include_filename = '0';
             }
 
             zen_copy_products_attributes($products_id_from, $products_id_to);
 }
 // EOF: Attributes Copy on non-linked
 /////////////////////////////////////////////////////////////////////
-
             // copy product discounts to duplicate
-            if ($_POST['copy_discounts'] == 'copy_discounts_yes') {
+    if (!empty($_POST['copy_discounts']) && $_POST['copy_discounts'] == 'copy_discounts_yes') {
               zen_copy_discounts_to_product($old_products_id, (int)$dup_products_id);
             }
 
+    zen_record_admin_activity('Product ' . (int)$old_products_id . ' duplicated as product ' . (int)$dup_products_id . ' via admin console.', 'info');
           }
 
           // reset products_price_sorter for searches etc.
           zen_update_products_price_sorter($products_id);
-
         }
-        zen_redirect(zen_href_link(FILENAME_CATEGORIES, 'cPath=' . $categories_id . '&pID=' . $products_id . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '')));
+zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . $categories_id . '&pID=' . $products_id . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '')));
