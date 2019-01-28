165e37908fd26a39afb7b6bba384a12e6d38a808
mesnitu <dasstech@gmail.com>
2019-01-28 12:21:18 +0000

    added canonical. Note: wating for ceon update for zc156 to add more code.

--- ZC_INSTALLATION/includes/auto_loaders/config.bookx.php
+++ ZC_INSTALLATION/includes/auto_loaders/config.bookx.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the ZenCart add-on Book X which
  * introduces a new product type for books to the Zen Cart
- * shop system. Tested for compatibility on ZC v. 1.5
+ * shop system. Tested for compatibility on ZC v. 1.56a
  *
  * For latest version and support visit:
  * https://sourceforge.net/p/zencartbookx
@@ -14,18 +14,10 @@
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License V2.0
  *
- * @version BookX V 0.9.4-revision8 BETA
- * @version $Id: [ZC INSTALLATION]/includes/auto_loaders/config.bookx.php 2016-02-02 philou $
+ * @version BookX V 1.0.0
+ * @version $Id: [ZC INSTALLATION]/includes/auto_loaders/config.bookx.php 2019-01-30 mesnitu $
  */
 
-/**
- * autoloader array for bookx
- * Registers the file [ZC INSTALLATION]/includes/classes/observers/class.bookx_observers.php
- * so it can listen to notifier hooks
- * 
- * see  {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
- *
-  */
 if (!defined('IS_ADMIN_FLAG')) {
  die('Illegal Access');
 }
@@ -35,3 +27,13 @@ if (!defined('IS_ADMIN_FLAG')) {
 	$autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
 			'className'=>'productTypeFilterObserver',
 			'objectName'=>'productTypeFilterObserver');
+    
+    $autoLoadConfig[10][] = array('autoType'=>'class',
+			'loadFile'=>'observers/class.bookx_observers_canonical.php');
+	
+	$autoLoadConfig[161][] = array('autoType'=>'classInstantiate',
+			'className'=>'bookxCanonicalObserver',
+			'objectName'=>'bookxCanonicalObserver');
+    
+//    $autoLoadConfig[80][] = array('autoType'=>'init_script',
+//                                 'loadFile'=> 'init_bookx.php');
